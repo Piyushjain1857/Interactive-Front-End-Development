d8586ef06f3dad7fd14d957b278abcb704d118e2
Piyush <piyushjain1857@gmail.com>
2026-01-14 10:04:38 +0530

    new map example

--- JS-Day-2/script.js
+++ JS-Day-2/script.js
@@ -1,26 +1,30 @@
 // For Loop Example: Add 3 to each element in the array
-let arr = [1, 2, 3, 4, 5, 6]
-for (i = 0; i < arr.length; i++) {
-    arr[i] = arr[i] + 3;
-}
-console.log(arr);
+
+// let arr = [1, 2, 3, 4, 5, 6]
+// for (i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i] + 3;
+// }
+// console.log(arr);
 
 // ForEach Example: Add 3 to each element in the array
-let arr2 = [1, 2, 3, 4, 5, 6]
-arr2.forEach(function (element, index, array) {
-    array[index] = element + 3;
-});
-console.log(arr2);
+
+// let arr2 = [1, 2, 3, 4, 5, 6]
+// arr2.forEach(function (element, index, array) {
+//     array[index] = element + 3;
+// });
+// console.log(arr2);
 
 
 // Map Example: Create a new array with each element increased by 3
-let arr3 = [1, 2, 3, 4, 5, 6]
-let newArr = arr3.map((i) => {
-    return i + 3;
-});
-console.log(newArr);
+
+// let arr3 = [1, 2, 3, 4, 5, 6]
+// let newArr = arr3.map((i) => {
+//     return i + 3;
+// });
+// console.log(newArr);
 
 // Nested For Loop Example
+
 // for (let i = 1; i <= 5; i++) {
 //     for (let j = 1; j <= i; j++) {
 //         console.log(j);
@@ -28,3 +32,6 @@ console.log(newArr);
 //     console.log(i);
 // }
 
+let arr = [1, 2, 3, 4, 5]
+let result = arr.map((x) => { return x; });
+console.log(result);
