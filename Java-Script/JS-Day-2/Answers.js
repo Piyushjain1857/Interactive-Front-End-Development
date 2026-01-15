bdfcace9f9a63be4878bff9579d3e40d17bad0f4
Piyush <piyushjain1857@gmail.com>
2026-01-15 20:36:23 +0530

    Answer-2

--- Java-Script/JS-Day-2/Answers.js
+++ Java-Script/JS-Day-2/Answers.js
@@ -1,5 +1,12 @@
+// Q1. Iterate all the elements and print the elements.
 const elements = [10, 20, 30, 40, 50];
 
 for (let i = 0; i < elements.length; i++) {
     console.log(elements[i]);
 }
+// Q2. Add 5 to each number inside the array.Take Arrey Length 5.
+const numbers = [10, 20, 30, 40, 50];
+for (let i = 0; i < numbers.length; i++) {
+    numbers[i] = numbers[i] + 5;
+}
+console.log(numbers);
