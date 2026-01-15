5799b0a667b59dd2271559168a53996d5a7a5a08
Piyush <piyushjain1857@gmail.com>
2026-01-15 20:38:29 +0530

    Answer-3

--- Java-Script/JS-Day-2/Answers.js
+++ Java-Script/JS-Day-2/Answers.js
@@ -5,8 +5,17 @@ for (let i = 0; i < elements.length; i++) {
     console.log(elements[i]);
 }
 // Q2. Add 5 to each number inside the array.Take Arrey Length 5.
-const numbers = [10, 20, 30, 40, 50];
-for (let i = 0; i < numbers.length; i++) {
-    numbers[i] = numbers[i] + 5;
+const arr = [10, 20, 30, 40, 50];
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = arr[i] + 5;
 }
-console.log(numbers);
+console.log(arr);
+
+// Q3. Reverce The Arrey. The elements should be numeric
+const nums = [1, 2, 3, 4, 5];
+const reversedNums = [];
+for (let i = nums.length - 1; i >= 0; i--) {
+    reversedNums.push(nums[i]);
+}
+console.log(reversedNums);
+
