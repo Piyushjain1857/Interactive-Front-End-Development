5107278fa30db6ade0b437dd77f36549547131b9
Piyush <piyushjain1857@gmail.com>
2026-01-16 19:47:15 +0530

    push

---
+++ Java-Script/Day-3/script.js
@@ -0,0 +1,8 @@
+// Push
+//  let fruits = ["apple", "banana"];
+//  let newLength = fruits.push("cherry");
+
+//  console.log(newLength);  //3
+//  console.log(fruits) // ['apple', 'banana', 'cherry']
+
+
