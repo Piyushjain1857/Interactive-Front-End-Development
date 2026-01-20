30916c4d3b43236c45a3fe214c33a13728b690f0
Piyush <piyushjain1857@gmail.com>
2026-01-20 09:49:59 +0530

    test

---
+++ Java-Script/JS-Day-4/script.js
@@ -0,0 +1,19 @@
+// let numbers=[1,2,3,4,5]
+// let sliced = numbers.slice(1,3)
+// console.log(sliced)
+
+// let number = [1, 2, 3, 4, 5]
+// let has_even = number.some(x => x % 2 === 0)
+// console.log(has_even)
+
+
+// let number = [1, 2, 3, 4, 5]
+// let has_even = number.every(x => x % 2 === 0)
+// console.log(has_even)
+
+
+// let array1 = [1, 2, 3];
+// let array2 = [4, 5, 6];
+// let concatenated = array1.concat(array2);
+// console.log(concatenated);
+
