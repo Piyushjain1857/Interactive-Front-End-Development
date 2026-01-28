370cda17dcbae18ba0c8b18118c66b84b81bdb71
Piyush <piyushjain1857@gmail.com>
2026-01-28 09:40:19 +0530

    added string literals

--- Java-Script/JS-Day-7/script.js
+++ Java-Script/JS-Day-7/script.js
@@ -6,10 +6,14 @@
 
 let user={
     name:"Piyush",
-    age:21,
+    age:18,
     skills:["HTML","CSS","JS"],
     addres:"Gurugram",
     favcolor:["red","blue","green"]
 }
 
 console.log(user);
+console.log("My Name Is "+user.name+". I am "+user.age+" years old.");
+
+// string literals
+console.log(`My Name Is ${user.name}. I am ${user.age} years old.`);
