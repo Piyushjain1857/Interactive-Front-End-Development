8be32c0bff4bb45ad2f13c54c5a5461fc09f7668
Piyush <piyushjain1857@gmail.com>
2026-01-30 15:49:26 +0530

    add styling script for headings in style.js and update index.html

---
+++ Java-Script/JS-Day-8/style.js
@@ -0,0 +1,16 @@
+// const heading = document.getElementById("title");
+// console.log(heading);
+// heading.style.color = "blue";
+// heading.style.backgroundColor = "lightblue";
+// heading.style.textAlign = "center";
+// heading.textContent = "Welcome to JavaScript Styling!";
+
+const heading = document.getElementsByClassName("title");
+console.log(heading);
+
+for (let i = 0; i < heading.length; i++) {
+    heading[i].style.color = "blue";
+    heading[i].style.backgroundColor = "lightblue";
+    heading[i].style.textAlign = "center";
+    heading[i].textContent = "Welcome to JavaScript Styling!";
+}
