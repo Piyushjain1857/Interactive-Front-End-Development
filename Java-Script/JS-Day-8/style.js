1912378e1f957d9c3a14484ad123fa2faf269365
Piyush <piyushjain1857@gmail.com>
2026-01-30 20:22:02 +0530

    textContent

--- Java-Script/JS-Day-8/style.js
+++ Java-Script/JS-Day-8/style.js
@@ -12,4 +12,5 @@ for (let i = 0; i < heading.length; i++) {
     heading[i].style.color = "blue";
     heading[i].style.backgroundColor = "lightblue";
     heading[i].style.textAlign = "center";
+    heading[i].textContent = "Welcome to JavaScript Styling!";
 }
