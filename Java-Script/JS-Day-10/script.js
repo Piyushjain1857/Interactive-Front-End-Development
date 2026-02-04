ad6739cc2f33078d794ed7e08a4133ebba604583
Piyush <piyushjain1857@gmail.com>
2026-02-04 09:46:13 +0530

    added some elements

--- Java-Script/JS-Day-10/script.js
+++ Java-Script/JS-Day-10/script.js
@@ -1,6 +1,6 @@
 const head = document.getElementById('heading');
 
-head.innerHTML = "<i>New Heading</i>"
+// head.innerHTML = "<i>New Heading</i>"
 console.log(head.innerHTML); // It will Show only the visible text
 
 console.log(head.textContent); // It will Show all the text including hidden text and HTML tags
