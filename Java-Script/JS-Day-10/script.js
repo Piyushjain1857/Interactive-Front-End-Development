e02b340ca4a58294a1a6caf376a98d0dec5d87ed
Piyush <piyushjain1857@gmail.com>
2026-02-04 10:22:33 +0530

    added newPhara

--- Java-Script/JS-Day-10/script.js
+++ Java-Script/JS-Day-10/script.js
@@ -1,8 +1,17 @@
-const head = document.getElementById('heading');
+// const head = document.getElementById('heading');
 
-// head.innerHTML = "<i>New Heading</i>"
-console.log(head.innerHTML); // It will Show only the visible text
+// // head.innerHTML = "<i>New Heading</i>"
+// console.log(head.innerHTML); // It will Show only the visible text
 
-console.log(head.textContent); // It will Show all the text including hidden text and HTML tags
+// console.log(head.textContent); // It will Show all the text including hidden text and HTML tags
 
-console.log(head.innerText); // It will Show only the visible text without any HTML tags
+// console.log(head.innerText); // It will Show only the visible text without any HTML tags
+
+// don mofification
+const newPhara = document.createElement('p');
+newPhara.textContent = "This is a dynamically created paragraph"; 
+console.log(newPhara.innerHTML); 
+
+// append the new element to the container
+const container = document.getElementById('container');
+container.appendChild(newPhara);
