6b39b5a1607bbd0610236ef0e29254277a510a29
Piyush <piyushjain1857@gmail.com>
2026-02-08 21:15:06 +0530

    Q3

--- Java-Script/JS-Day-11/script.js
+++ Java-Script/JS-Day-11/script.js
@@ -22,3 +22,30 @@
 
 //     document.getElementById("content").append(image);
 // })
+
+
+
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", function () {
+
+    const link = document.createElement("a");
+    link.href = "https://www.krmangalam.edu.in/";
+    link.innerText = "Visit KR Mangalam University";
+
+    const box = document.createElement("div");
+    box.innerText = "Welcome to KR Mangalam University";
+
+    box.style.backgroundColor = "tomato";
+    box.style.borderRadius = "20px";
+    box.style.height = "150px";
+    box.style.display = "flex";
+    box.style.alignItems = "center";
+    box.style.justifyContent = "center";
+    box.style.color = "white";
+    box.style.fontSize = "18px";
+
+    output.appendChild(link);
+    output.appendChild(box);
+});
