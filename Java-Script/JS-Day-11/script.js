28286b0f1afaad2f03ad1ee2522b2a6921acaf39
Piyush <piyushjain1857@gmail.com>
2026-02-08 21:08:38 +0530

    code commented

--- Java-Script/JS-Day-11/script.js
+++ Java-Script/JS-Day-11/script.js
@@ -13,11 +13,12 @@
 
 //     document.getElementById("content").append(image);
 // }
-document.getElementById("btn").addEventListener("click",()=>{
-    const image = document.createElement("img");
-    image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
 
-    image.setAttribute("alt", "kr mangalam");
+// document.getElementById("btn").addEventListener("click",()=>{
+//     const image = document.createElement("img");
+//     image.setAttribute("src", "https://krmangalam.edu.in/_next/image?url=%2Flife-at-krmu%2Finfra.webp&w=3840&q=75")
 
-    document.getElementById("content").append(image);
-})
+//     image.setAttribute("alt", "kr mangalam");
+
+//     document.getElementById("content").append(image);
+// })
