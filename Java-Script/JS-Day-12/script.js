e88a301bb7c0925f8876c5f5f2b7cd05b1666b50
Piyush <piyushjain1857@gmail.com>
2026-02-10 09:39:24 +0530

    creaded form

--- Java-Script/JS-Day-12/script.js
+++ Java-Script/JS-Day-12/script.js
@@ -1,6 +1,13 @@
-const nameInput = document.getElementById("myInput");
-nameInput.addEventListener("change", () => {
-    document.getElementById("output").textContent = "Changed code";
-    console.log(nameInput.value);
+// document.getElementById("myInput").addEventListener("change", ()=> {
+//     document.getElementById("output").textContent = "Code Changed By Piyush Jain";
+// });
 
+const nameInput = document.getElementById("myInput");
+const selectInput = document.getElementById("mySelect");
+const button = document.getElementById("btn");
+const output = document.getElementById("output");
+
+button.addEventListener("click", () => {
+    output.textContent = `Input value: ${nameInput.value}, Selected value: ${selectInput.value}`;
+    console.log(nameInput.value, selectInput.value);
 });
