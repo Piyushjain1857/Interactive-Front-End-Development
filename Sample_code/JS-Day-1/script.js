eadbcf9210ed1e0a651c86e9719ff3399679d8bc
Piyush <piyushjain1857@gmail.com>
2026-02-19 14:29:31 +0530

    code commented

--- Sample_code/JS-Day-1/script.js
+++ Sample_code/JS-Day-1/script.js
@@ -1,11 +1,11 @@
-console.log("Start...");
-function myFunction(cb) {
-    setTimeout(() => {
-        cb("Long running task finished.");
-    }, 2000); 
-}
-// myFunction();
-myFunction((result) => {
-    console.log(result);
-});
-console.log("End...");
+// console.log("Start...");
+// function myFunction(cb) {
+//     setTimeout(() => {
+//         cb("Long running task finished.");
+//     }, 2000); 
+// }
+// // myFunction();
+// myFunction((result) => {
+//     console.log(result);
+// });
+// console.log("End...");
