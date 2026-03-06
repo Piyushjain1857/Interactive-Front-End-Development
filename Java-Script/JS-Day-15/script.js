6421269e9ca3336e2c989f9f8ee3464f64dde05c
Piyush <piyushjain1857@gmail.com>
2026-03-06 14:54:27 +0530

    add more promise

--- Java-Script/JS-Day-15/script.js
+++ Java-Script/JS-Day-15/script.js
@@ -11,15 +11,19 @@ setTimeout(() => {
             }, 1000);
         }, 1000);
     }, 1000);
-}, 1000);        
+}, 1000);
 
 // promise
 
 function printAfterDelay(num, delay) {
-    return new Promise((resolve) => {
+    return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log(num);
-            resolve();
+            if (num === 3) {
+                reject("Error: Failed while printing " + num);
+            } else {
+                console.log(num);
+                resolve();
+            }
         }, delay);
     });
 }
@@ -27,4 +31,26 @@ function printAfterDelay(num, delay) {
 printAfterDelay(1, 1000)
     .then(() => printAfterDelay(2, 1000))
     .then(() => printAfterDelay(3, 1000))
-    .then(() => printAfterDelay(4, 1000));  
+    .then(() => printAfterDelay(4, 1000))
+    .catch(error => console.error("Promise Rejected:", error));
+
+// promice with fetch
+
+fetch("https://jsonplaceholder.typicode.com/posts/1")
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
+
+
+new Promise((resolve, rejected) => {
+    let payment = "True";
+    if (payment == "True") {
+        resolve("Payment Successfull");
+    } else {
+        rejected("Payment Failed");
+    }
+}).then((message) => {
+    console.log(message);
+}).catch((error) => {
+    console.error(error);
+});
