9d95ad331a7ad21b98f7b9c6c1c561e857977fea
Piyush <piyushjain1857@gmail.com>
2026-03-06 14:42:24 +0530

    added callback hell

---
+++ Java-Script/JS-Day-15/script.js
@@ -0,0 +1,15 @@
+// callback hell
+
+setTimeout(() => {
+    console.log("1");
+    setTimeout(() => {
+        console.log("2");
+        setTimeout(() => {
+            console.log("3");
+            setTimeout(() => {
+                console.log("4");
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);        
+
