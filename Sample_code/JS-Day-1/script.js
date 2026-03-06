51b3062ac9cabb5262e5186698dd2bc4a05375b3
Piyush <piyushjain1857@gmail.com>
2026-03-06 14:25:40 +0530

    added code for practice

--- Sample_code/JS-Day-1/script.js
+++ Sample_code/JS-Day-1/script.js
@@ -9,3 +9,33 @@
 //     console.log(result);
 // });
 // console.log("End...");
+
+
+// console.log("Start");
+
+// function heavyTask() {
+//     for (let i = 0; i < 1e9; i++) { }
+// }
+
+// function heavyTask() {
+//     for (let i = 0; i < 1e9; i++) { }
+// }
+// function heavyTask() {
+//     for (let i = 0; i < 1e9; i++) { }
+// }
+// function heavyTask() {
+//     for (let i = 0; i < 1e9; i++) { }
+// }
+// function heavyTask() {
+//     for (let i = 0; i < 1e9; i++) { }
+// }
+// heavyTask();
+// console.log("End");
+
+setTimeout(() => {
+    console.log("Hello Piyush Runned after 2 seconds");
+}, 2000);
+
+setInterval(() => {
+    // console.log("Hello Piyush Runned every 1 seconds");
+}, 1000);
