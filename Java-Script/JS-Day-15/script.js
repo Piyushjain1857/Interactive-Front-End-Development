19c2af45b730d64c93fdb7f974b415dee0597f13
Piyush <piyushjain1857@gmail.com>
2026-03-06 14:42:35 +0530

    added promice

--- Java-Script/JS-Day-15/script.js
+++ Java-Script/JS-Day-15/script.js
@@ -13,3 +13,18 @@ setTimeout(() => {
     }, 1000);
 }, 1000);        
 
+// promise
+
+function printAfterDelay(num, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            console.log(num);
+            resolve();
+        }, delay);
+    });
+}
+
+printAfterDelay(1, 1000)
+    .then(() => printAfterDelay(2, 1000))
+    .then(() => printAfterDelay(3, 1000))
+    .then(() => printAfterDelay(4, 1000));  
