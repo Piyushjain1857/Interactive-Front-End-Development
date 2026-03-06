002d594a05f34f8bbbc0a57ad24c29b2e9c3daa3
Piyush <piyushjain1857@gmail.com>
2026-03-06 15:22:11 +0530

    output guessing

--- Java-Script/JS-Day-15/script.js
+++ Java-Script/JS-Day-15/script.js
@@ -1,56 +1,87 @@
-// callback hell
+// // callback hell
 
-setTimeout(() => {
-    console.log("1");
-    setTimeout(() => {
-        console.log("2");
-        setTimeout(() => {
-            console.log("3");
-            setTimeout(() => {
-                console.log("4");
-            }, 1000);
-        }, 1000);
-    }, 1000);
-}, 1000);
+// setTimeout(() => {
+//     console.log("1");
+//     setTimeout(() => {
+//         console.log("2");
+//         setTimeout(() => {
+//             console.log("3");
+//             setTimeout(() => {
+//                 console.log("4");
+//             }, 1000);
+//         }, 1000);
+//     }, 1000);
+// }, 1000);
 
-// promise
+// // promise
 
-function printAfterDelay(num, delay) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (num === 3) {
-                reject("Error: Failed while printing " + num);
-            } else {
-                console.log(num);
-                resolve();
-            }
-        }, delay);
-    });
-}
+// function printAfterDelay(num, delay) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             if (num === 3) {
+//                 reject("Error: Failed while printing " + num);
+//             } else {
+//                 console.log(num);
+//                 resolve();
+//             }
+//         }, delay);
+//     });
+// }
 
-printAfterDelay(1, 1000)
-    .then(() => printAfterDelay(2, 1000))
-    .then(() => printAfterDelay(3, 1000))
-    .then(() => printAfterDelay(4, 1000))
-    .catch(error => console.error("Promise Rejected:", error));
+// printAfterDelay(1, 1000)
+//     .then(() => printAfterDelay(2, 1000))
+//     .then(() => printAfterDelay(3, 1000))
+//     .then(() => printAfterDelay(4, 1000))
+//     .catch(error => console.error("Promise Rejected:", error));
 
-// promice with fetch
+// // promice with fetch
 
-fetch("https://jsonplaceholder.typicode.com/posts/1")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error("Error:", error));
+// fetch("https://jsonplaceholder.typicode.com/posts/1")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.error("Error:", error));
 
 
-new Promise((resolve, rejected) => {
-    let payment = "True";
-    if (payment == "True") {
-        resolve("Payment Successfull");
-    } else {
-        rejected("Payment Failed");
-    }
-}).then((message) => {
-    console.log(message);
-}).catch((error) => {
-    console.error(error);
-});
+// new Promise((resolve, rejected) => {
+//     let payment = "True";
+//     if (payment == "True") {
+//         resolve("Dashboard Accessed");
+//     } else {
+//         rejected("Dashboard Access Denied");
+//     }
+// }).then((message) => {
+//     console.log(message);
+// }).catch((error) => {
+//     console.error(error);
+// });
+
+
+// function pro() {
+//     return new Promise((resolve, rejected) => {
+//         let payment = "True";
+//         if (payment == "True") {
+//             resolve("Dashboard Accessed");
+//         } else {
+//             rejected("Dashboard Access Denied");
+//         }
+//     })
+// }
+
+// pro().then((message) => {
+//     console.log(message);
+// }).catch((error) => {
+//     console.error(error);
+// });
+
+
+// console.log("start");
+// setTimeout(() => {
+//     console.log("timeout");
+// }, 0);
+
+// Promise.resolve().then(() => {
+//     console.log("promise");
+// });
+
+// console.log("end");
+
